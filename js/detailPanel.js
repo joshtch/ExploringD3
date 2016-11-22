ecb55da12786de1671f5eee6c3dd7dddb1eada14
Nick Usoff <nicholas.usoff@tufts.edu>
2016-11-22 11:14:46 -0500

    adding code description to sidebar

--- js/detailPanel.js
+++ js/detailPanel.js
@@ -3,17 +3,40 @@ var txt = "";
 
 	var TITLE = "#dc-title";
 	var TEXT = "#dc-code-text";
+	var DESC = "#dc-description";
 
-function updateDetails (d) {
-	console.log(d)
-	var name = d.name;
-	$(TITLE).text(name);
+	function updateDetails (d) {
+		
+		var name = d.name;
+		$(TITLE).text(name);
 
-	var path = "dataset/d3-core-js/" + name + ".js";
+		var path = "dataset/d3-core-js/" + name + ".js";
 
-	//TODO check to see if this is a file
+		//TODO check to see if this is a
 
-	getFileText(path).then(function(fileText) {
-		$(TEXT).html(jQuery.parseHTML(fileText))
-	});
+		getFileText(path).then(function(fileText) {
+			$(TEXT).html(jQuery.parseHTML(fileText))
+		});
+
+		var readmePath = "https://raw.githubusercontent.com/d3/" + name + "/master/README.md"
+
+		getReadme(readmePath).then(function(fileHTML) {
+			$(DESC).html(fileHTML);
+		})
+	}
+
+
+function getReadme (file) {
+	console.log("getReadme called")
+	return new Promise(function(resolve, reject) {
+        var req = new XMLHttpRequest();
+            req.onreadystatechange = function() {
+                if (req.readyState == XMLHttpRequest.DONE) {
+                    resolve(req.responseText);
+                }
+            }
+            req.onerror = reject;
+            req.open("GET", file);
+            req.send();
+        });
 }
