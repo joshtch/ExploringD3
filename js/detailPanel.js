19abc83ab01edffe37d8bdcad3257a236972d9ce
Nick Usoff <nicholas.usoff@tufts.edu>
2016-11-28 13:24:01 -0500

    readme of parent displayed when function selected now

--- js/detailPanel.js
+++ js/detailPanel.js
@@ -14,13 +14,21 @@ var DESC = "#dc-description";
 
 		var path = "dataset/d3-core-js/" + name + ".js";
 
-		//TODO check to see if this is a
+		//TODO check to see if this is a 
 
 		getFileText(path).then(function(fileText) {
 			$(TEXT).html(jQuery.parseHTML(fileText))
 		});
 
-		var readmePath = "https://raw.githubusercontent.com/d3/" + name + "/master/README.md"
+		var readmeName = d.name;
+
+		if(d.type == "function"){
+			readmeName = d.parent.name
+		}
+
+		console.log(readmeName);
+
+		var readmePath = "https://raw.githubusercontent.com/d3/" + readmeName + "/master/README.md"
 
 		getReadme(readmePath).then(function(fileHTML) {
 			var parsedReadme = parseReadme(fileHTML)
