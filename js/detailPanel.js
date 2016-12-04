0ee5db41c054744e76c73db5df96db17095adc64
Nick Usoff <nicholas.usoff@tufts.edu>
2016-12-04 14:10:02 -0500

    readme parsing

--- js/detailPanel.js
+++ js/detailPanel.js
@@ -1,4 +1,3 @@
-// Constants for id's of elements within the detail-container
 var txt = "";
 
 jQuery.fn.scrollTo = function(elem) {
@@ -6,9 +5,14 @@ jQuery.fn.scrollTo = function(elem) {
     return this;
 };
 
+// Constants for id's of elements within the detail-container
 var TITLE = "#dc-title";
 var TEXT = "#dc-code-text";
 var DESC = "#dc-description";
+
+$("#parentModule").click(function(d){
+	console.log("clicked");
+});
 
 function updateDetails (d) {
 
@@ -16,35 +20,63 @@ function updateDetails (d) {
     $("#tree-svg").attr("width", $("#tree-container").width());
 	$("#detail-container").css("visibility", "visible");
 
-	var name = d.name;
-	$(TITLE).text(name);
-
-	var path = "dataset/d3-core-js/" + name + ".js";
-
 	var readmeName = d.name;
+	var path = "dataset/d3-core-js/" + readmeName + ".js";
+
 
 	if(d.type == "function"){
 		readmeName = d.parent.name
 	}
-
-	console.log(readmeName);
-
 	var readmePath = "https://raw.githubusercontent.com/d3/" + readmeName + "/master/README.md"
 
 	getReadme(readmePath).then(function(fileHTML) {
-		var parsedReadme = parseReadme(fileHTML);
+		var parsedReadme = extractReadmePart(fileHTML, d.type, readmeName, d.name)
 
 		$(DESC).html(parsedReadme);
-		var tagSelector
-		if(d.type == "function") {
-			tagSelector = 'a[name=' + d.name + ']'
-		} else {
-			tagSelector = "#" + d.name.replace(/-/g, "")
-		}
-		$("#dc-description-container").scrollTo(tagSelector);
+		var tagSelector;
+		// if(d.type == "function") {
+		// 	tagSelector = 'a[name=' + d.name + ']'
+		// } else {
+		// 	tagSelector = "#" + d.name.replace(/-/g, "")
+		// }
+		// console.log(tagSelector)
+		// $("#dc-description-container").scrollTo(tagSelector);
 	})
 
 	updateCode(path);
+}
+
+function extractReadmePart(readme, fileType, moduleName, functionName) {
+	var parsedReadme = parseReadme(readme);
+	if(fileType == "module") {
+		var tryItLink = '<p><a href="https://tonicdev.com/npm/' + moduleName + 
+		                '" target="_blank">Try it out!</a></p>';
+		txt = parsedReadme;
+		endOfDescription = parsedReadme.search('<h2 id="installing"');
+		if(endOfDescription != -1){
+			return parsedReadme.slice(0, endOfDescription) + tryItLink
+		}
+		// console.log(parsedReadme + tryItLink)
+		return parsedReadme + tryItLink;
+	} 
+	// fileType is "function" otherwise
+
+	var descriptionStart = '#</a> d3.<b>' + functionName.trim();
+	var apiReferenceStartInd = parsedReadme.search(descriptionStart);
+
+	var apiReference = parsedReadme.slice(apiReferenceStartInd);
+	var apiReferenceEndInd = apiReference.indexOf('<h2', 1);
+	apiReference = apiReference.slice(0, apiReferenceEndInd);
+	// var node = d3.findOne()
+
+	
+
+	var head = '<h3><a id="parentModule">' + moduleName.trim() + '</a>/' + functionName.trim() + '</h3><hr/>'
+	return head + apiReference;
+}
+
+function dummy () {
+	console.log("h");
 }
 
 function closeDetails() {
